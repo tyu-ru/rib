c07d7f6e9cad192e200f6dd8bfa2305c82085450
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-03-13 21:12:45 +0900

    feat: UniqueType

--- Type/UniqueType.hpp
+++ Type/UniqueType.hpp
@@ -1,40 +1,40 @@
 #pragma once
 
 /// provide unique type
-namespace rib::uniquetype
+namespace rib::type
 {
 
 /// unique type 0
-struct Type0
+struct UniqueType0
 {
 };
 /// unique type 1
-struct Type1
+struct UniqueType1
 {
 };
 /// unique type 2
-struct Type2
+struct UniqueType2
 {
 };
 /// unique type 3
-struct Type3
+struct UniqueType3
 {
 };
 /// unique type 4
-struct Type4
+struct UniqueType4
 {
 };
 /// unique type 5
-struct Type5
+struct UniqueType5
 {
 };
 /// unique type 6
-struct Type6
+struct UniqueType6
 {
 };
 /// unique type 7
-struct Type7
+struct UniqueType7
 {
 };
 
-} // namespace rib::uniquetype
+} // namespace rib::type
