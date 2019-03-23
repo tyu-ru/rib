077f71f85e224955d84c5ccd8263d8ee6214f0be
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-03-23 12:49:55 +0900

    feat: algorithm_util

---
+++ algorithm_util.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <algorithm>
+#include <functional>
+
+namespace rib::algorithm
+{
+
+/// xxx_if algorithm
+
+#define DECLARE_XXX_IF_ALGORITHM(func_name)                                                                                   \
+    template <class InputIterator, class F, class T>                                                                          \
+    inline constexpr auto func_name(InputIterator first, InputIterator last, F fn, T&& value)                                 \
+    {                                                                                                                         \
+        return std::func_name(first, last, [&](auto&& v) { return std::invoke(fn, std::forward<decltype(v)>(v)) == value; }); \
+    }                                                                                                                         \
+    template <class Container, class F, class T>                                                                              \
+    inline constexpr auto func_name(Container&& c, F fn, T&& value)                                                           \
+    {                                                                                                                         \
+        using std::begin, std::end;                                                                                           \
+        return func_name(begin(c), end(c), fn, std::forward<T>(value));                                                       \
+    }
+
+DECLARE_XXX_IF_ALGORITHM(all_of)
+DECLARE_XXX_IF_ALGORITHM(any_of)
+DECLARE_XXX_IF_ALGORITHM(none_of)
+DECLARE_XXX_IF_ALGORITHM(find_if)
+DECLARE_XXX_IF_ALGORITHM(find_if_not)
+DECLARE_XXX_IF_ALGORITHM(count_if)
+
+#undef DECLARE_XXX_IF_ALGORITHM
+
+/// container wrapper
+
+template <class Container, class T>
+inline constexpr auto find(Container&& c, T&& value)
+{
+    using std::begin, std::end;
+    return std::find(begin(c), end(c), value);
+}
+
+} // namespace rib::algorithm
