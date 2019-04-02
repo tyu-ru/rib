b3c7274e26ac86eab4b260f70ba9d47c3a3d5826
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-04-02 19:47:23 +0900

    feat: less compare

--- Dimension/Quantity.hpp
+++ Dimension/Quantity.hpp
@@ -1,25 +1,28 @@
 #pragma once
 
 #include <utility>
-#include <functional>
 #include "Dimension.hpp"
 #include "../Type/TypeTraits.hpp"
+#include "../functional_util.hpp"
 
 namespace rib::units
 {
 
-template <class Dimention, class V = double>
+template <class Dimention, class Value = double>
 class Quantity
 {
     static_assert(type::is_template_specialized_by_value_v<dim::Dimension, Dimention>);
 
+    template <class D, class V>
+    friend class Quantity;
+
 public:
     using DimensionType = Dimention;
-    using ValueType = V;
+    using ValueType = Value;
 
 public:
     constexpr Quantity() = default;
-    explicit constexpr Quantity(const V& other) : val(other) {}
+    explicit constexpr Quantity(const ValueType& other) : val(other) {}
     constexpr Quantity(const Quantity&) = default;
     constexpr Quantity(Quantity&&) = default;
     ~Quantity() = default;
@@ -27,14 +30,14 @@ public:
     constexpr Quantity& operator=(const Quantity&) = default;
     constexpr Quantity& operator=(Quantity&&) = default;
 
-    template <class V2>
-    constexpr Quantity& operator=(const Quantity<DimensionType, V2>& rhs)
+    template <class V>
+    constexpr Quantity& operator=(const Quantity<DimensionType, V>& rhs)
     {
         val = rhs.val;
         return *this;
     }
-    template <class V2>
-    constexpr Quantity& operator=(Quantity<DimensionType, V2>&& rhs)
+    template <class V>
+    constexpr Quantity& operator=(Quantity<DimensionType, V>&& rhs)
     {
         val = std::move(rhs.val);
         return *this;
@@ -44,6 +47,12 @@ public:
     constexpr ValueType& value() & { return val; }
     constexpr const ValueType&& value() const&& { return std::move(val); }
     constexpr ValueType&& value() && { return std::move(val); }
+
+    template <class V>
+    constexpr bool operator<(const Quantity<DimensionType, V>& rhs) const
+    {
+        return val < rhs.val;
+    }
 
 private:
     ValueType val{};
@@ -65,5 +74,11 @@ static_assert(std::is_assignable_v<Quantity<dim::Dimension<>, int>&, Quantity<di
 
 static_assert(std::is_assignable_v<Quantity<dim::Dimension<>, int>&, Quantity<dim::Dimension<1>, int>> == false);
 static_assert(std::is_assignable_v<Quantity<dim::Dimension<>, int>&, int> == false);
+
+static_assert(func::is_lessable_r_v<bool, Quantity<dim::Dimension<>, int>, Quantity<dim::Dimension<>, int>>);
+static_assert(func::is_lessable_r_v<bool, Quantity<dim::Dimension<>, int>, Quantity<dim::Dimension<>, long>>);
+static_assert(func::is_lessable_r_v<bool, Quantity<dim::Dimension<>, int>, Quantity<dim::Dimension<1>, int>> == false);
+
+static_assert(Quantity<dim::Dimension<>, int>{1} < Quantity<dim::Dimension<>, long>{2});
 
 } // namespace rib::units
---
+++ functional_util.hpp
@@ -0,0 +1,56 @@
+#include <functional>
+#include <type_traits>
+
+namespace rib::func
+{
+
+#define DECLARE_COMPOUND_OPERATOR_FUNCTOR(name, op)                                                \
+    template <class T = void>                                                                      \
+    struct name                                                                                    \
+    {                                                                                              \
+        constexpr T& operator()(T& lhs, const T& rhs) const { return lhs op rhs; }                 \
+    };                                                                                             \
+    template <>                                                                                    \
+    struct name<void>                                                                              \
+    {                                                                                              \
+        template <class T, class U>                                                                \
+        constexpr auto operator()(T&& lhs, U&& rhs) const -> decltype(lhs op std::forward<U>(rhs)) \
+        {                                                                                          \
+            return lhs op std::forward<U>(rhs);                                                    \
+        }                                                                                          \
+    };
+
+DECLARE_COMPOUND_OPERATOR_FUNCTOR(compound_plus, +=)
+DECLARE_COMPOUND_OPERATOR_FUNCTOR(compound_minus, -=)
+DECLARE_COMPOUND_OPERATOR_FUNCTOR(compound_multiplies, *=)
+DECLARE_COMPOUND_OPERATOR_FUNCTOR(compound_divides, /=)
+DECLARE_COMPOUND_OPERATOR_FUNCTOR(compound_modulus, %=)
+
+#undef DECLARE_COMPOUND_OPERATOR_FUNCTOR
+
+#define DECLARE_OPERATOR_ABLE_R_V(name, functor)                       \
+    template <class R, class T, class U>                               \
+    struct is_##name##able_r : std::is_invocable_r<R, functor<>, T, U> \
+    {                                                                  \
+    };                                                                 \
+    template <class R, class T, class U>                               \
+    constexpr bool is_##name##able_r_v = is_##name##able_r<R, T, U>::value;
+
+DECLARE_OPERATOR_ABLE_R_V(plus, std::plus)
+DECLARE_OPERATOR_ABLE_R_V(minus, std::minus)
+DECLARE_OPERATOR_ABLE_R_V(multiplies, std::multiplies)
+DECLARE_OPERATOR_ABLE_R_V(divides, std::divides)
+DECLARE_OPERATOR_ABLE_R_V(modulus, std::modulus)
+
+DECLARE_OPERATOR_ABLE_R_V(compound_plus, compound_plus)
+
+DECLARE_OPERATOR_ABLE_R_V(equal_to, std::equal_to)
+DECLARE_OPERATOR_ABLE_R_V(not_equal_to, std::not_equal_to)
+DECLARE_OPERATOR_ABLE_R_V(less, std::less)
+DECLARE_OPERATOR_ABLE_R_V(less_equal, std::less_equal)
+DECLARE_OPERATOR_ABLE_R_V(greater, std::greater)
+DECLARE_OPERATOR_ABLE_R_V(greater_equal, std::greater_equal)
+
+#undef DECLARE_OPERATOR_ABLE_R_V
+
+} // namespace rib::func
