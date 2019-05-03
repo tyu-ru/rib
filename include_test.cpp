864d0569cfa2cd7d2b508de1718fdbd87808286e
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-05-03 17:13:01 +0900

    feat: create Expected

---
+++ include_test.cpp
@@ -0,0 +1,11 @@
+
+#include "Reference.hpp"
+#include "Lambda.hpp"
+
+// container
+#include "Container/Optional.hpp"
+#include "Container/Expected.hpp"
+
+#include "Units/Quantity.hpp"
+
+int main() { return 0; }
