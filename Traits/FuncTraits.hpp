fa29d44d3abec479f54c1aa52f7d354cb78ddd31
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-05-04 17:06:26 +0900

    chore: Traits

--- Traits/FuncTraits.hpp
+++ Traits/FuncTraits.hpp
@@ -3,7 +3,7 @@
 #include <functional>
 #include <type_traits>
 
-namespace rib::func
+namespace rib::traits
 {
 
 template <typename T = void>
@@ -115,4 +115,70 @@ DECLARE_BINARY_OPERATOR_ABLE(greater_equal, std::greater_equal)
 
 #undef DECLARE_BINARY_OPERATOR_ABLE
 
-} // namespace rib::func
+/**
+ * @brief type to which the member belong
+ */
+template <class>
+struct belong_type
+{
+    using type = void;
+};
+template <class T, class U>
+struct belong_type<U T::*>
+{
+    using type = T;
+};
+template <class R, class T, class... Args>
+struct belong_type<R (T::*)(Args...)>
+{
+    using type = T;
+};
+template <class T>
+using belong_type_t = typename belong_type<T>::type;
+
+static_assert(std::is_same_v<belong_type_t<int(int)>, void>);
+static_assert([] {
+    struct A
+    {
+    };
+    return std::is_same_v<belong_type_t<int A::*>, A>;
+}());
+static_assert([] {
+    struct A
+    {
+    };
+    return std::is_same_v<belong_type_t<int (A::*)(int)>, A>;
+}());
+
+/**
+ * @brief strict invoke result type check
+ * @tparam R result type
+ * @tparam F invokeable type
+ * @tparam Args 
+ */
+template <class R, class F, class... Args>
+struct is_result
+    : std::is_same<
+          R,
+          typename std::conditional_t<
+              std::is_member_function_pointer_v<F> || std::is_member_object_pointer_v<F>,
+              std::invoke_result<F, belong_type_t<F>, Args...>,
+              std::invoke_result<F, Args...>>::type>
+{
+};
+template <class R, class F, class... Args>
+constexpr bool is_result_v = is_result<R, F, Args...>::value;
+
+static_assert(is_result_v<int, int(int), int>);
+static_assert(!is_result_v<int, long(int), int>);
+
+template <class F, class... Args>
+inline constexpr std::invoke_result_t<F, Args...> invoke_constexpr(F&& f, Args&&... args) noexcept(std::is_nothrow_invocable_v<F, Args...>)
+{
+    if constexpr (std::is_member_pointer_v<F>) {
+        return std::invoke(std::forward<F>(f), std::forward<Args>(args)...);
+    } else {
+        return f(std::forward<Args>(args)...);
+    }
+}
+} // namespace rib::traits
--- Traits/TypeTraits.hpp
+++ Traits/TypeTraits.hpp
@@ -3,7 +3,7 @@
 #include <type_traits>
 
 /// type utility
-namespace rib::type
+namespace rib::traits
 {
 
 /**
@@ -68,69 +68,26 @@ constexpr bool is_template_specialized_by_value_v = is_template_specialized_by_v
 
 /**
  * @brief copy const qualification
- * @tparam T 
- * @tparam U 
+ * @tparam From
+ * @tparam To
  */
-template <class T, class U>
-struct copy_const : std::conditional<std::is_const_v<T>,
-                                     const std::remove_const_t<U>,
-                                     std::remove_const_t<U>>
+template <class From, class To>
+struct copy_const : std::conditional<std::is_const_v<From>,
+                                     const std::remove_const_t<To>,
+                                     std::remove_const_t<To>>
 {
 };
 /**
  * @brief helper alias template: copy_const
- * @tparam T 
- * @tparam U 
+ * @tparam T
+ * @tparam U
  */
 template <class T, class U>
 using copy_const_t = typename copy_const<T, U>::type;
 
-/**
- * @brief type to which the member belong
- */
-template <class>
-struct belong_type
-{
-    using type = void;
-};
-template <class T, class U>
-struct belong_type<U T::*>
-{
-    using type = T;
-};
-template <class R, class T, class... Args>
-struct belong_type<R (T::*)(Args...)>
-{
-    using type = T;
-};
-template <class T>
-using belong_type_t = typename belong_type<T>::type;
+static_assert(std::is_same_v<copy_const_t<int, long>, long>);
+static_assert(std::is_same_v<copy_const_t<const int, long>, const long>);
+static_assert(std::is_same_v<copy_const_t<int, const long>, long>);
+static_assert(std::is_same_v<copy_const_t<const int, const long>, const long>);
 
-static_assert(std::is_same_v<belong_type_t<int(int)>, void>);
-
-/**
- * @brief strict invoke result type check
- * @tparam R result type
- * @tparam F invokeable type
- * @tparam Args 
- */
-template <class R, class F, class... Args>
-struct is_result
-    : std::is_same<
-          R,
-          typename std::conditional_t<
-              std::is_member_function_pointer_v<F> || std::is_member_object_pointer_v<F>,
-              std::invoke_result<F, belong_type_t<F>, Args...>,
-              std::invoke_result<F, Args...>>::type>
-{
-};
-template <class R, class F, class... Args>
-constexpr bool is_result_v = is_result<R, F, Args...>::value;
-
-static_assert(is_result_v<int, int(int), int>);
-static_assert(!is_result_v<int, long(int), int>);
-
-#define DECLV(T) std::declval<T>()
-#define RESULT_T(T, F) decltype(DECLV(T).F)
-
-} // namespace rib::type
+} // namespace rib::traits
