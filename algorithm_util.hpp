dafe94336a13bcfcb1954f4062c85b0245ccbe68
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-05-08 06:05:06 +0900

    refactor: use invoke_constexpr in algorithm

--- algorithm_util.hpp
+++ algorithm_util.hpp
@@ -1,24 +1,25 @@
 #pragma once
 
 #include <algorithm>
-#include <functional>
+#include "Traits/FuncTraits.hpp"
 
 namespace rib::algorithm
 {
 
 /// xxx_if algorithm
 
-#define DECLARE_XXX_IF_ALGORITHM(func_name)                                                                                   \
-    template <class InputIterator, class F, class T>                                                                          \
-    inline constexpr auto func_name(InputIterator first, InputIterator last, F fn, T&& value)                                 \
-    {                                                                                                                         \
-        return std::func_name(first, last, [&](auto&& v) { return std::invoke(fn, std::forward<decltype(v)>(v)) == value; }); \
-    }                                                                                                                         \
-    template <class Container, class F, class T>                                                                              \
-    inline constexpr auto func_name(Container&& c, F fn, T&& value)                                                           \
-    {                                                                                                                         \
-        using std::begin, std::end;                                                                                           \
-        return func_name(begin(c), end(c), fn, std::forward<T>(value));                                                       \
+#define DECLARE_XXX_IF_ALGORITHM(func_name)                                                                                  \
+    template <class InputIterator, class F, class T>                                                                         \
+    inline constexpr auto func_name(InputIterator first, InputIterator last, F fn, T&& value)                                \
+    {                                                                                                                        \
+        return std::func_name(first, last,                                                                                   \
+                              [&](auto&& v) { return trait::invoke_constexpr(fn, std::forward<decltype(v)>(v)) == value; }); \
+    }                                                                                                                        \
+    template <class Container, class F, class T>                                                                             \
+    inline constexpr auto func_name(Container&& c, F fn, T&& value)                                                          \
+    {                                                                                                                        \
+        using std::begin, std::end;                                                                                          \
+        return func_name(begin(c), end(c), fn, std::forward<T>(value));                                                      \
     }
 
 DECLARE_XXX_IF_ALGORITHM(all_of)
