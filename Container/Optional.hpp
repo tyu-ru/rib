575e4b5cddba46b83d7ad7d748dc86125587c2ba
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-05-12 12:48:50 +0900

    refactor: rename Optional::unwrap

--- Container/Optional.hpp
+++ Container/Optional.hpp
@@ -22,23 +22,23 @@ class Optional
     static constexpr bool is_nested = trait::is_template_specialized_by_type_v<Optional, T>;
 
     template <class U>
-    struct deepest
+    struct unwrap_nest
     {
         using type = std::remove_reference_t<U>;
     };
     template <class U>
-    struct deepest<Optional<U>>
+    struct unwrap_nest<Optional<U>>
     {
         using type = std::remove_reference_t<U>;
     };
     template <class U>
-    struct deepest<std::optional<U>>
+    struct unwrap_nest<std::optional<U>>
     {
         using type = std::remove_reference_t<U>;
     };
 
     template <class U>
-    using deepest_t = typename deepest<U>::type;
+    using unwrap_nest_t = typename unwrap_nest<U>::type;
 
 public:
     template <class... Args, trait::concept_t<std::is_constructible_v<std::optional<T>, Args...>> = nullptr>
@@ -46,7 +46,7 @@ public:
     {
     }
 
-    constexpr auto unwrap() const -> Optional<deepest_t<T>>
+    constexpr auto unwrap() const -> Optional<unwrap_nest_t<T>>
     {
         if constexpr (is_nested) {
             return *this;
@@ -56,7 +56,7 @@ public:
     }
 
     template <class F, class... Args>
-    constexpr auto map(F&& f, Args&&... args) const& -> Optional<deepest_t<std::invoke_result_t<F, T, Args...>>>
+    constexpr auto map(F&& f, Args&&... args) const& -> Optional<unwrap_nest_t<std::invoke_result_t<F, T, Args...>>>
     {
         if (this->has_value()) {
             return trait::invoke_constexpr(std::forward<F>(f), **this, std::forward<Args>(args)...);
@@ -64,7 +64,7 @@ public:
         return std::nullopt;
     }
     template <class F, class... Args>
-    constexpr auto map(F&& f, Args&&... args) & -> Optional<deepest_t<std::invoke_result_t<F, T, Args...>>>
+    constexpr auto map(F&& f, Args&&... args) & -> Optional<unwrap_nest_t<std::invoke_result_t<F, T, Args...>>>
     {
         if (this->has_value()) {
             return trait::invoke_constexpr(std::forward<F>(f), **this, std::forward<Args>(args)...);
@@ -72,7 +72,7 @@ public:
         return std::nullopt;
     }
     template <class F, class... Args>
-    constexpr auto map(F&& f, Args&&... args) && -> Optional<deepest_t<std::invoke_result_t<F, T, Args...>>>
+    constexpr auto map(F&& f, Args&&... args) && -> Optional<unwrap_nest_t<std::invoke_result_t<F, T, Args...>>>
     {
         if (this->has_value()) {
             return trait::invoke_constexpr(std::forward<F>(f), std::move(**this), std::forward<Args>(args)...);
@@ -81,17 +81,17 @@ public:
     }
 
     template <class F>
-    constexpr auto operator|(F&& f) const& -> Optional<deepest_t<std::invoke_result_t<F, T>>>
+    constexpr auto operator|(F&& f) const& -> Optional<unwrap_nest_t<std::invoke_result_t<F, T>>>
     {
         return map(std::forward<F>(f));
     }
     template <class F>
-    constexpr auto operator|(F&& f) & -> Optional<deepest_t<std::invoke_result_t<F, T>>>
+    constexpr auto operator|(F&& f) & -> Optional<unwrap_nest_t<std::invoke_result_t<F, T>>>
     {
         return map(std::forward<F>(f));
     }
     template <class F>
-    constexpr auto operator|(F&& f) && -> Optional<deepest_t<std::invoke_result_t<F, T>>>
+    constexpr auto operator|(F&& f) && -> Optional<unwrap_nest_t<std::invoke_result_t<F, T>>>
     {
         return map(std::forward<F>(f));
     }
--- include_test.cpp
+++ include_test.cpp
@@ -8,4 +8,15 @@
 
 #include "Units/Quantity.hpp"
 
-int main() { return 0; }
+#include <iostream>
+
+int main()
+{
+    rib::Optional<int> a(1);
+
+    a.visit(VISITOR(x, x += 1;));
+
+    std::cout << a.value_or(0) << " " << a.map([](int x) { return x + 1; }).value_or(0) << std::endl;
+
+    return 0;
+}
