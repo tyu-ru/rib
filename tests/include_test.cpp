61015284a694be91208fb7ee17e0a641f73f0d5f
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-06-24 01:57:33 +0900

    chore: dir ajust

---
+++ tests/include_test.cpp
@@ -0,0 +1,16 @@
+
+#include "Utils/Reference.hpp"
+#include "Utils/Lambda.hpp"
+
+// container
+#include "Container/Optional.hpp"
+#include "Container/Expected.hpp"
+
+#include "Units/Quantity.hpp"
+
+#include "Iterator/MaybeIterator.hpp"
+#include "Container/MaybeRange.hpp"
+
+#include "Utils/CompareCat.hpp"
+
+int main() {}
