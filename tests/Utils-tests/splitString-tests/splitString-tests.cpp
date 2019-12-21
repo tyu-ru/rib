9dbfcc8ab980a9a784544f9114e22a7c7ea46c46
tyu-ru <tyuru.tw.cpp@gmail.com>
2019-12-21 18:59:34 +0900

    refactor: splitString tests

--- tests/Utils-tests/splitString-tests/splitString-tests.cpp
+++ tests/Utils-tests/splitString-tests/splitString-tests.cpp
@@ -9,45 +9,39 @@ using namespace rib;
 
 TEST_CASE("splitString", "[utils]")
 {
-    std::vector<std::string> v, e;
+    using VS = std::vector<std::string>;
+    using record = std::tuple<std::string, std::string, std::string, VS>;
+    auto r = GENERATE(table<std::string, std::string, std::string, VS>({
+        record{"empty", "", "", VS{}},
+        record{"empty delimiter", "Hello World", "", VS{}},
+        record{"no match", "Hello World", ",", VS{"Hello World"}},
+        record{"normal1", "Hello World", " ", VS{"Hello", "World"}},
+        record{"normal2", "Hello Programming World", " ", VS{"Hello", "Programming", "World"}},
+        record{"multi delimiter", "Hello Programming,World", " ,", VS{"Hello", "Programming", "World"}},
+    }));
+
+    DYNAMIC_SECTION(std::get<0>(r))
+    {
+        std::vector<std::string> v;
+        splitString(std::get<1>(r), std::get<2>(r), VISITOR_CAP(s, v.emplace_back(s);));
+        CHECK(v == std::get<3>(r));
+    }
+}
+
+TEST_CASE("splitString skip empty", "[util]")
+{
+    std::vector<std::string> v;
+    using VS = std::vector<std::string>;
     auto lmd = VISITOR_CAP(s, v.emplace_back(s););
-    SECTION("empty string")
+
+    SECTION("skip")
     {
-        splitString("", "", lmd);
-        e.assign({});
+        splitString("Hello  Programming World", " ", lmd);
+        CHECK(v == VS{"Hello", "Programming", "World"});
     }
-    SECTION("normal1")
+    SECTION("keep")
     {
-        splitString("Hello World", " ", lmd);
-        e.assign({"Hello", "World"});
+        splitString("Hello  Programming World", " ", lmd, false);
+        CHECK(v == VS{"Hello", "", "Programming", "World"});
     }
-    SECTION("normal2")
-    {
-        splitString("Hello Programming World", " ", lmd);
-        e.assign({"Hello", "Programming", "World"});
-    }
-    SECTION("empty delimiter")
-    {
-        splitString("Hello Programming World", "", lmd);
-        e.assign({});
-    }
-    SECTION("skip empty")
-    {
-        SECTION("skip")
-        {
-            splitString("Hello  Programming World", " ", lmd);
-            e.assign({"Hello", "Programming", "World"});
-        }
-        SECTION("keep")
-        {
-            splitString("Hello  Programming World", " ", lmd, false);
-            e.assign({"Hello", "", "Programming", "World"});
-        }
-    }
-    SECTION("multi delimiter")
-    {
-        splitString("Hello Programming,World", " ,", lmd);
-        e.assign({"Hello", "Programming", "World"});
-    }
-    CHECK(v == e);
 }
